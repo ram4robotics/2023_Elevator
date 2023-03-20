2c3122f24d777422cb73d5de5798875d0b1795a5
R S Burugula <burugula@us.ibm.com>
2023-03-20 13:20:24 -0400

    Initial Commit
    Inlione command based Elevator Subsystem

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static class CAN_IDs {
+    public static final int driveTrain_Left1 = 11;
+    public static final int driveTrain_Left2 = 12;
+    public static final int driveTrain_Right1 = 13;
+    public static final int driveTrain_Right2 = 14;
+    public static final int intakeWheels = 21;
+    public static final int intakeArm = 22;
+    public static final int elevatorLeft = 23;
+    public static final int elevatorRight = 24;
+    public static final int slider = 25;
+  }
+  public static class ElevatorConstants {
+    public static final boolean kMotorLeft_inverted = false;
+    public static final boolean kMotorRight_inverted = false;
+    public static final int kCurrentLimit = 40;
+    public static final double kElevatorGearRatio = 1 / (5 * 4); // 5:1 * 4:1
+    // RevRobotics 16T Sprocket pitch size is 1.29 inches
+    // https://www.revrobotics.com/25-sprockets/
+    // https://www.revrobotics.com/content/docs/REV-21-2016-DR.pdf
+    // kPositionFactor converts #motor-rotations into #inches travelled by the Elevator
+    public static final double kPositionFactor = Math.PI * 1.29 * kElevatorGearRatio;
+    // kVelocityFactor converts Motor-RPM (Revolutions Per Minute) into  inches-per-second
+    public static final double kVelocityFactor = kPositionFactor / 60;
+
+    public static final double kMinTravelInInches = 0;
+    public static final double kMaxTravelInInches = 36; // ToDo:  Verify with experimentation
+    public static final double kToleranceInInches = 1.8; // 5% of travel length
+    public static final double kElevatorSpeedUp = 0.7;
+    public static final double kElevatorSpeedDown = -0.3;
+  }
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort1 = 0;
+    public static final int kDriverControllerPort2 = 1;
+    public static final double kDriveDeadband = 0.05;
+    public static final double kArmManualDeadband = 0.05;
+    public static final double kArmManualScale = 0.5;
+  }}
---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,121 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.CAN_IDs;
+import frc.robot.Constants.ElevatorConstants;
+
+public class Elevator extends SubsystemBase {
+  private final CANSparkMax m_motorLeft, m_motorRight;
+  private final RelativeEncoder m_encoderLeft, m_encoderRight;
+
+  /** Creates a new Elevator. */
+  public Elevator() {
+    m_motorLeft = new CANSparkMax(CAN_IDs.elevatorLeft, MotorType.kBrushless);
+    m_motorLeft.setInverted(ElevatorConstants.kMotorLeft_inverted);
+    m_motorLeft.setIdleMode(IdleMode.kBrake);
+    m_motorLeft.setSmartCurrentLimit(ElevatorConstants.kCurrentLimit);
+    m_motorLeft.burnFlash();
+
+    m_motorRight = new CANSparkMax(CAN_IDs.elevatorRight, MotorType.kBrushless);
+    m_motorRight.setInverted(ElevatorConstants.kMotorRight_inverted);
+    m_motorRight.setIdleMode(IdleMode.kBrake);
+    m_motorRight.setSmartCurrentLimit(ElevatorConstants.kCurrentLimit);
+    m_motorRight.burnFlash();
+
+    m_encoderLeft = m_motorLeft.getEncoder();
+    m_encoderLeft.setPositionConversionFactor(ElevatorConstants.kPositionFactor);
+    m_encoderLeft.setVelocityConversionFactor(ElevatorConstants.kVelocityFactor);
+    
+    m_encoderRight = m_motorRight.getEncoder();
+    m_encoderRight.setPositionConversionFactor(ElevatorConstants.kPositionFactor);
+    m_encoderRight.setVelocityConversionFactor(ElevatorConstants.kVelocityFactor);
+    resetEncoders();
+
+    SmartDashboard.putData(this);
+  }
+
+  private void resetEncoders() {
+    m_encoderLeft.setPosition(0);
+    m_encoderRight.setPosition(0);
+  }
+
+  private void setSpeed(double speed) {
+    m_motorLeft.set(speed);
+    m_motorRight.set(speed);
+  }
+
+  private boolean elevatorIsNotSafe() {
+    double leftPosition = m_encoderLeft.getPosition();
+    double rightPosition = m_encoderRight.getPosition();
+    return ((leftPosition < ElevatorConstants.kMinTravelInInches) ||
+          (leftPosition > ElevatorConstants.kMaxTravelInInches) ||
+          (rightPosition < ElevatorConstants.kMinTravelInInches) ||
+          (rightPosition > ElevatorConstants.kMaxTravelInInches));
+  }
+
+  public boolean elevatorIsAtHeight(double height) {
+    double curHeight = (m_encoderLeft.getPosition() + m_encoderRight.getPosition()) / 2;
+    return (Math.abs(curHeight - height) < ElevatorConstants.kToleranceInInches);
+  }
+
+  public CommandBase raise() {
+    return this.runOnce(() -> setSpeed(ElevatorConstants.kElevatorSpeedUp))
+              .unless(() -> elevatorIsNotSafe());
+  }
+
+  public CommandBase lower() {
+    return this.runOnce(() -> setSpeed(ElevatorConstants.kElevatorSpeedDown))
+              .unless(() -> elevatorIsNotSafe());
+  }
+
+  public CommandBase raiseToHeight(double desiredHeight) {
+    return this.run(() -> setSpeed(ElevatorConstants.kElevatorSpeedUp))
+              .unless(() -> (elevatorIsNotSafe() ||
+                      (desiredHeight <= getCurHeight()) ||
+                      elevatorIsAtHeight(desiredHeight)))
+              .until(() -> elevatorIsAtHeight(desiredHeight))
+              .finallyDo((interrupted) -> setSpeed(0))
+              .withName("raiseToHeight");
+  }
+
+  public CommandBase lowerToHeight(double desiredHeight) {
+    return this.run(() -> setSpeed(ElevatorConstants.kElevatorSpeedDown))
+              .unless(() -> (elevatorIsNotSafe() || 
+                            desiredHeight >= getCurHeight() ||
+                            elevatorIsAtHeight(desiredHeight)))
+              .until(() -> elevatorIsAtHeight(desiredHeight))
+              .finallyDo((interrupted) -> setSpeed(0))
+              .withName("lowerToHeight");
+  }
+
+  private double getCurHeight() {
+    return (m_encoderLeft.getPosition() + m_encoderRight.getPosition()) / 2;
+  }
+
+  public CommandBase setHeight(double desiredHeight) {
+    return new ConditionalCommand(raiseToHeight(desiredHeight), 
+                                  lowerToHeight(desiredHeight), 
+                                  () -> (desiredHeight > getCurHeight()))
+                .unless(() -> Math.abs(getCurHeight() - desiredHeight) < ElevatorConstants.kToleranceInInches)
+                .finallyDo((interrupted) -> setSpeed(0));
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("LeftPosition", m_encoderLeft.getPosition());
+    SmartDashboard.putNumber("RightPosition", m_encoderRight.getPosition());
+  }
+}
